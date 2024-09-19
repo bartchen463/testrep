ced924022ff2cc942ae79d4bf3dbd332935ce363
Bart Chen <bart@entergrid.com>
2024-09-19 15:23:32 -0600

    Lab 4

--- Labs/Lab 4/Lab 4.py
+++ Labs/Lab 4/Lab 4.py
@@ -22,15 +22,38 @@ def fixedpt(f, x0, tol, Nmax):
     return x, xstar, ier
 
 
-f1 = lambda x: -np.sin(2 *x) + (5 * x) / 4 - 3 / 4
+
+def compute_order(x, xstar):
+    diff1 = np.abs(x[1:] - xstar)
+    diff2 = np.abs(x[0:-1] - xstar)
+
+    fit = np.polyfit(np.log(diff2.flatten()), np.log(diff1.flatten()), 1)
+    _lambda = np.exp(fit[0])
+    alpha = fit[0]
+    print(f"Lambda is {_lambda}")
+    print(f"Alpha is {alpha}")
+    return fit
+
+
+f1 = lambda x: np.sqrt(10/(x+4))
 
 
 Nmax = 1000
 tol = 1e-10
 
 # test f1 '''
-x0 = 2.5
+x0 = 1.5
 [x, xstar, ier] = fixedpt(f1, x0, tol, Nmax)
 print('the approximate fixed point is:', xstar)
 print('f1(xstar):', f1(xstar))
 print('Error message reads:', ier)
+
+compute_order(x, 1.3652300134140976)
+
+def Aitken(x):
+    phats = [x[i] - ((x[i+1] - x[i]) ** 2)/(x[i+2] - 2*x[i+1] + x[i]) for i in range(len(x) - 2)]
+    return np.array(phats)
+
+(Aitken(x) - 1.3652300134140976)/(x - 1.3652300134140976)[:-2]
+
+compute_order(np.array([m for m in Aitken(x) if m!=0]), 1.3652300134140976)
